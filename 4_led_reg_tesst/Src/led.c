dd3af7a6a136a04ee081ff2ae08c207c8fe201d5
jcagua <Jonathan.cagua@easymetering.com>
2022-12-22 18:19:25 -0500

    FIX: files

--- 4_led_reg_tesst/Src/led.c
+++ 4_led_reg_tesst/Src/led.c
@@ -13,20 +13,32 @@
 #include "led.h"
 //Data sheet Table 10, Peripheral RCC
 //See 6.3.9 RM0383
-#define RCC_AHB1ENR 	(*(volatile uint32_t *)(0x40023830))
-#define GPIOD_RCC_AHB1ENR (1 << 3)
+#define PERIPH_BASE			(0X40000000UL)
+#define AHB1_PERIPH_OFFSET	(0X00020000UL)
+#define GPIOD_OFFSET		(0X0C00UL)
+#define RCC_OFFSET			(0X3800UL)
+
+#define RCC_AHB1ENR_OFFSET	(0x30UL)
+
+
+#define AHB1_PERIPH_BASE	(PERIPH_BASE + AHB1_PERIPH_OFFSET)
+
+#define GPIOD_BASE			(AHB1_PERIPH_BASE + GPIOD_OFFSET)
+#define RCC_BASE			(AHB1_PERIPH_BASE + RCC_OFFSET)
+
+#define RCC_AHB1ENR 		(*(volatile uint32_t *)(RCC_BASE+RCC_AHB1ENR_OFFSET))
+#define GPIOD_RCC_AHB1ENR 	(1 << 3)
 //Data sheet Table 10, Peripheral GPIOD
 //See GPIO registers 8.4
-#define AHB1_GPIOD 		0x40020c00 //
-#define GPIOD_MODER 	(*(volatile uint32_t *)(AHB1_GPIOD + 0x00))
-#define GPIOD_OTYPE 	(*(volatile uint32_t *)(AHB1_GPIOD + 0x04))
-#define GPIOD_OTYPE 	(*(volatile uint32_t *)(AHB1_GPIOD + 0x04))
-#define GPIOD_PUPDR     (*(volatile uint32_t *)(AHB1_GPIOD + 0x0c))
-#define GPIOD_ODR  		(*(volatile uint32_t *)(AHB1_GPIOD + 0x14))
-#define GPIOD_BSRR 		(*(volatile uint32_t *)(AHB1_GPIOD + 0x18))
-#define LED_PIN 		(12)
-#define MODER_LEN		(2)
-#define LED_SELECT		(LED_PIN * MODER_LEN)
+#define GPIOD_MODER 		(*(volatile uint32_t *)(GPIOD_BASE + 0x00))
+#define GPIOD_OTYPE 		(*(volatile uint32_t *)(GPIOD_BASE + 0x04))
+#define GPIOD_OTYPE 		(*(volatile uint32_t *)(GPIOD_BASE + 0x04))
+#define GPIOD_PUPDR     	(*(volatile uint32_t *)(GPIOD_BASE + 0x0c))
+#define GPIOD_ODR  			(*(volatile uint32_t *)(GPIOD_BASE + 0x14))
+#define GPIOD_BSRR 			(*(volatile uint32_t *)(GPIOD_BASE + 0x18))
+#define LED_PIN 			(12)
+#define MODER_LEN			(2)
+#define LED_SELECT			(LED_PIN * MODER_LEN)
 void led_setup(void)
 {
     uint32_t reg;
